bbb1d4629a7fe721ae059665d79defe0088a0613
yash-parashar <77394386+yash-parashar@users.noreply.github.com>
2021-07-06 17:38:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+
+if(contestantCount===2){
+  quiz.update(1)
+  counterCount:0
+
+}
+
+if(gameState===1){
+  clear()
+  quiz.play()
+  gameSate:0
+  
+}
+
+
+
+
+
+
+
+
+}
